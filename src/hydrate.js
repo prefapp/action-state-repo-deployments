9ea38b0a0f0daf60737682ba6a7876f107b1e586
Jose Alvarez <jose.alvarez@prefapp.es>
2024-11-06 11:29:23 +0100

    Run prettier

--- src/hydrate.js
+++ src/hydrate.js
@@ -1,47 +1,50 @@
-function createNestedObject(arr, val) { return arr.reduceRight((acc, key) => ({ [key]: acc }), val); };
+function createNestedObject(arr, val) {
+  return arr.reduceRight((acc, key) => ({ [key]: acc }), val)
+}
 
-function hydrateDeployment(path, ca_keys = ['councilbox-server', 'ca_secret', 'crts']) {
+function hydrateDeployment(
+  path,
+  ca_keys = ['councilbox-server', 'ca_secret', 'crts']
+) {
+  const fs = require('node:fs')
+  const path = require('node:path')
+  const glob = require('node:glob')
 
-    const fs = require('node:fs');
-    const path = require('node:path');
-    const glob = require('node:glob');
+  const excluded_files = ['secrets.yaml', 'images.yaml', 'ca.yml', 'final.yaml']
 
-    const excluded_files = ['secrets.yaml', 'images.yaml', 'ca.yml', 'final.yaml']
+  const yaml_files = glob.sync(path.join(path, '*.yaml'))
+  const filtered_files = yaml_files.filter(
+    f => !excluded_files.includes(path.basename(f))
+  )
 
-    const yaml_files = glob.sync(path.join(path, '*.yaml'))
-    const filtered_files = yaml_files.filter(f => !excluded_files.includes(path.basename(f)))
+  const final_yaml = path.join(path, 'final.yaml')
 
-    const final_yaml = path.join(path, 'final.yaml')
+  const final_yaml_stream = fs.createWriteStream(final_yaml, { flags: 'w' })
 
-    const final_yaml_stream = fs.createWriteStream(final_yaml, { flags: 'w' })
+  filtered_files.forEach(fname => {
+    const content = fs.readFileSync(fname, 'utf-8')
+    final_yaml_stream.write(content)
+  })
 
-    filtered_files.forEach(fname => {
-        const content = fs.readFileSync(fname, 'utf-8')
-        final_yaml_stream.write(content)
-    })
+  final_yaml_stream.end()
 
-    final_yaml_stream.end()
+  console.log(fs.readFileSync(final_yaml, 'utf-8'))
 
-    console.log(fs.readFileSync(final_yaml, 'utf-8'))
+  const ca_files = glob.sync(path.join(path, 'ca-certs', '*.crt'))
 
-    const ca_files = glob.sync(path.join(path, "ca-certs", '*.crt'))
+  let ca = ''
 
-    let ca = ''
+  ca_files.forEach(file => {
+    ca += fs.readFileSync(file, 'utf-8')
+  })
 
-    ca_files.forEach(file => {
-        ca += fs.readFileSync(file, 'utf-8')
-    })
+  const dict_file = createNestedObject(ca_keys, ca)
 
+  const ca_yaml = path.join(path, 'ca.yml')
 
-    const dict_file = createNestedObject(ca_keys, ca)
+  const ca_yaml_stream = fs.createWriteStream(ca_yaml, { flags: 'w' })
 
-    const ca_yaml = path.join(path, "ca.yml")
+  ca_yaml_stream.write(JSON.stringify(dict_file))
 
-
-    const ca_yaml_stream = fs.createWriteStream(ca_yaml, { flags: 'w' })
-
-    ca_yaml_stream.write(JSON.stringify(dict_file))
-
-    ca_yaml_stream.end()
-
+  ca_yaml_stream.end()
 }
