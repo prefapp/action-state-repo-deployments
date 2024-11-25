86f42fdc67be561bf6f26816688d11877343f517
Jose Alvarez <jose.alvarez@prefapp.es>
2024-11-25 10:50:59 +0100

    Fix bug in config

--- dist/index.js
+++ dist/index.js
@@ -38387,19 +38387,22 @@ class Config {
    * @param {string} outputDir The directory to output the rendered deployments.
    * @param {string} argoProjectsDir The directory containing the ArgoCD projects.
    * @param {string} prNumber The pull request number.
+   * @param {string} author The author of the pull request.
    */
   constructor(
     environment,
     deploymentsDir,
     outputDir,
     argoProjectsDir,
-    prNumber
+    prNumber,
+    author
   ) {
     this.environment = environment
     this.deploymentsDir = deploymentsDir
     this.outputDir = outputDir
     this.argoProjectsDir = argoProjectsDir
     this.prNumber = prNumber
+    this.author = author
   }
 }
 
